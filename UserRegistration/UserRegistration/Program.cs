1ec4c7c183d20aee1890dba53903972fb6eb84cc
Ankush Bahekar <ankushmb9@gmail.com>
2022-03-26 11:34:16 +0530

    [Ankush] Add: Valid last name

--- UserRegistration/UserRegistration/Program.cs
+++ UserRegistration/UserRegistration/Program.cs
@@ -9,8 +9,8 @@ namespace UserRegistration
         {
             bool flag = true;
             while (flag)
-            {
-                Console.WriteLine("Enter Program number to get executed \n1. Validate first name \n2. Exit");
+             {
+                Console.WriteLine("Enter Program number to get executed \n1. Validate first name \n2. Validate last name \n3. Exit");
                 int option = Convert.ToInt32(Console.ReadLine());
                 switch (option)
                 {
@@ -19,11 +19,18 @@ namespace UserRegistration
                         Console.WriteLine("*length should be Minimum three characters.");
                         Console.WriteLine("Enter First name :");
                         string fname = Console.ReadLine();
-
                         Validation_regex Checkpattern = new Validation_regex();
                         Checkpattern.Checkfirstname(fname);
                         break;
                     case 2:
+                        Console.WriteLine("*First letter should be capital.");
+                        Console.WriteLine("*length should be Minimum three characters.");
+                        Console.WriteLine("Enter Last name :");
+                        string lname = Console.ReadLine();
+                        Validation_regex lnamepattern = new Validation_regex();
+                        lnamepattern.Checklastname(lname);
+                        break;
+                    case 3:
                         flag = false;
                         break;
                     default:
